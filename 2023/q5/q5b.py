765b523f6ff79c5b2b805e8121db58ad679013ab
unknown <skousboL@ualberta.ca>
2023-12-07 21:26:32 -0500

    q5 test case back to working

--- 2023/q5/q5b.py
+++ 2023/q5/q5b.py
@@ -1,21 +1,6 @@
 import re
 from datetime import datetime
 
-class SeedRange:
-  def __init__(self, range):
-    self.range = range
-    self.mapping = range
-    self.mapped = False
-  
-  def __str__(self):
-    return f"Seeds {self.range} maps to {self.mapping}"
-
-  def map(self, destination, source, range):
-
-    if not self.mapped and self.mapping >= source and self.mapping < (source + range):
-      self.mapping = self.mapping - source + destination
-      self.mapped = True
-
 def merge_ranges(value1, value2):
   a = max(value1[0], value2[0])
   b = min(value1[1], value2[1])
@@ -41,7 +26,6 @@ def merge_values(values):
     else:
       for j in range(len(sorted_values)):
         if values[i][0] > sorted_values[j][0]:
-          print('inc')
           place += 1
         else:
           break
@@ -55,14 +39,11 @@ def merge_values(values):
     merged = []
     for i in range(len(sorted_values)):
       for j in range(len(sorted_values)):
-        print(merged)
-        print(sorted_values)
         if j <= i or i in merged_indices or j in merged_indices:
           continue
         value1 = sorted_values[i]
         value2 = sorted_values[j]
         merged = merge_ranges(value1, value2)
-        print(value1, value2, merged)
         if len(merged) == 1:
           merged_indices.append(i)
           merged_indices.append(j)
@@ -74,7 +55,6 @@ def merge_values(values):
     if len(sorted_values) == len(merged_values):
       merge_occured = False
     sorted_values = merged_values
-    print(merged_values)
   
 
   return merged_values
@@ -111,43 +91,43 @@ def main():
 
   lowest_location = float('inf')
   for number in seed_numbers:
+    print('SEED')
     pair = number.split(' ')
     old_values = [[int(pair[0]), int(pair[0]) + int(pair[1]) - 1]]
+    unmapped_values = old_values
     values = []
     for resource_mapping in mappings:
       for mapping in resource_mapping:
-        print('-----------------------------')
-        print(mapping)
-
         unmapped_values = []
 
-        for old_value in old_values:
+        for i in range(len(old_values)):
+          old_value = old_values[i]
           a = max(mapping[0], old_value[0])
           b = min(mapping[1], old_value[1])
           overlap_range =range(a, b)
           if len(overlap_range) == 0:
             if old_value not in values:
-              values.append(old_value)
+              unmapped_values.append(old_value)
           else:
             # Add values not impacted as is
             # Add values impacted modified
-
             if [a + mapping[2], b + mapping[2]] not in values:
               values.append([a + mapping[2], b + mapping[2]])
-            if a > old_value[0] and [old_value[0], a] not in values:
-              values.append([old_value[0], a])
+            if a >= old_value[0] and [old_value[0], a] not in values:
+              unmapped_values.append([old_value[0], a])
             if b < old_value[1] and [b, old_value[1]] not in values:
-              values.append([b, old_value[1]])
-      print('BEFORE')
-      print(values)
-      print('-----------------------')
-      values = merge_values(values)
-      print('AFTER')
-      print(values)
+              unmapped_values.append([b, old_value[1]])
+      for value in unmapped_values:
+        values.append(value)
+
       old_values = values
       values = []
 
   print('END')
-  print(old_values)
+  for value_range in old_values:
+    for num in value_range:
+      if num < lowest_location:
+        lowest_location = num
+  print(lowest_location)
 
 main()
